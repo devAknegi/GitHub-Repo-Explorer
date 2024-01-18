85d7fb07a5520136c8d0acbcc975e7c0db54c4fc
devAknegi <ankushnegi42001@gmail.com>
2024-01-19 04:32:02 +0530

    Added Smooth Scrolling

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const githubForm = document.getElementById('github-form');
     const usernameInput = document.getElementById('username');
+
     githubForm.addEventListener('submit', function(event){
         event.preventDefault();
-        // const username = document.getElementById('username').value;
-        // console.log(username);
         const profile = document.querySelector('.profile');
         const username = usernameInput.value.trim();
         if(!username){
@@ -16,16 +15,15 @@ document.addEventListener('DOMContentLoaded', function () {
             fetch(`https://api.github.com/users/${username}`)
                 .then(response => response.json())
                 .then(user => {
-                    // Display user information
                     document.getElementById('profile_pic').src = user.avatar_url;
                     document.getElementById('username').innerText = user.login;
                     document.getElementById('name').innerText = user.name || '';
                     document.getElementById('bio').innerText = user.bio || '';
                     document.getElementById('location').innerText = user.location || '';
-                    document.getElementById('github-link').href = user.html_url;
+                    document.getElementById('link').href = user.html_url;
 
-                profile.classList.remove('d-none');
-                    // Fetch repositories
+                    profile.classList.remove('d-none');
+                    profile.scrollIntoView({behavior:'smooth'});
                     return fetch(`https://api.github.com/users/${username}/repos`);
                 })
                 .then(response => response.json())
